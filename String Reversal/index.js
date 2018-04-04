392bfba18d707727b0e4221022d87bcc6fcf3bf2
ivanevacic <ivanevacicweb@gmail.com>
2018-04-04 13:08:47 +0200

    Added Solution 1

---
+++ String Reversal/index.js
@@ -0,0 +1,20 @@
+/*
+
+Given a string, return a new string with the reversed order of characters
+
+    reverse('apple') -> 'elppa'
+
+
+*/
+
+//Solution 1
+/* function reverse(str){
+    //turn string to array of chars
+    const arr = str.split('');
+    //js function that reverses the order of chars
+    arr.reverse();
+    //returns array of chars back to string
+    return arr.join('');
+} */
+
+console.log(reverse('Ivan Evačić'));//ćičavE navI
