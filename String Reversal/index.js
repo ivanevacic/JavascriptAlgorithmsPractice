991cff8b1e0962348dd6a07a2172d2f700604366
ivanevacic <ivanevacicweb@gmail.com>
2018-04-04 13:16:26 +0200

    Added Solution 2

--- String Reversal/index.js
+++ String Reversal/index.js
@@ -2,19 +2,34 @@
 
 Given a string, return a new string with the reversed order of characters
 
-    reverse('apple') -> 'elppa'
+    revers('apple') -> 'elppa'
 
 
 */
 
 //Solution 1
-/* function reverse(str){
+
+/* function revers(str){
     //turn string to array of chars
     const arr = str.split('');
     //js function that reverses the order of chars
-    arr.reverse();
+    arr.revers();
     //returns array of chars back to string
     return arr.join('');
 } */
 
-console.log(reverse('Ivan Evačić'));//ćičavE navI
+//Solution 2
+
+/* function revers(str) {
+    //temporary empty variable
+    let reversed = '';
+    //loop trough every char in string
+    for (let character of str) {
+        //add each char to variable revers
+        reversed = character + reversed;
+    }
+    //return reversed variable
+    return reversed;
+} */
+
+console.log(revers('Ivan Evačić'));//ćičavE navI
